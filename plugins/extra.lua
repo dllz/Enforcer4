cc9f8d09bbd3bdda2c7140446cf6090bb1c3ff06
BladeZero <daniellotzsa@gmail.com>
2016-10-07 11:08:57 +0200

    Hot fix for extra with gif

--- plugins/extra.lua
+++ plugins/extra.lua
@@ -99,7 +99,6 @@ local action = function(msg, blocks, ln)
             	if special_method then
             		api.sendMediaId(msg.from.id, file_id, special_method) --photo, voices, video need their method to be sent by file_id
             	elseif hasMedia then
-				api.sendLog('hasMedia')
 					api.sendDocumentWithCapId(msg.from.id, hasMedia, text)
 				else
             		api.sendDocumentId(msg.from.id, file_id)
@@ -119,7 +118,6 @@ local action = function(msg, blocks, ln)
         			api.sendDocumentId(msg.chat.id, file_id, msg_to_reply)
         		end
         	elseif hasMedia ~= nil then
-				api.sendLog('hasMedia')
 				api.sendDocumentWithCapId(msg.from.id, hasMedia, text, msg_to_reply)
 			else
         		api.sendMessage(msg.chat.id, text, true, msg_to_reply) --if the mod replies to an user, the bot will reply to the user too
