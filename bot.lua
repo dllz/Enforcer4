7c2280e907b443ed13b031e88ba7fad8d6123790
BladeZero <daniellotzsa@gmail.com>
2016-10-07 22:47:32 +0200

    Changed processing limit to compensate for API issues

--- bot.lua
+++ bot.lua
@@ -141,7 +141,7 @@ on_msg_receive = function(msg) -- The fn run whenever a message is received.
 		return
 	end
 	
-	if msg.date < os.time() - 20 then return end -- Do not process old messages.
+	if msg.date < os.time() - 1800 then return end -- PRocess last 30 minutes
 	if not msg.text then msg.text = msg.caption or '' end
 	
 	msg.normal_group = false
