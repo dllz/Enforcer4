5d39385fc18963d9856e04a05212d1f27ca39a44
BladeZero <daniellotzsa@gmail.com>
2016-11-18 21:44:29 +0200

    logged on

--- bot.lua
+++ bot.lua
@@ -182,9 +182,9 @@ on_msg_receive = function(msg) -- The fn run whenever a message is received.
 		else
 			if plugin.triggers then
 				if (config.testing_mode and plugin.test) or not plugin.test then --run test plugins only if test mode it's on
+				print(msg.text)
 					for k,w in pairs(plugin.triggers) do
 						--print(w)
-						print(msg.text)
 						--print(k)
 						local blocks = match_pattern(w, msg.text)
 						if blocks then
