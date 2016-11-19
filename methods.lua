791e51a06707bc03385c4c6004aa0ced71413adf
BladeZero <daniellotzsa@gmail.com>
2016-11-19 19:59:52 +0200

    modified timeout

--- methods.lua
+++ methods.lua
@@ -77,7 +77,7 @@ end
 
 local function getUpdates(offset)
 
-	local url = BASE_URL .. '/getUpdates=20'
+	local url = BASE_URL .. '/getUpdates?timeout=20'
 
 	if offset then
 		url = url .. '&offset=' .. offset
